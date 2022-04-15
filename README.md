c2868141dc7844f0a1fd4fc5e533734eb949a21c
Искров Антон <iskrov.95@mail.ru>
2022-04-15 19:31:29 +0300

    first try

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# api_final
-api final
+# api_yatube
+#Авторский API для проекта Yatube 
+
+
+## Описание
+API учебного проекта Yatube, построенный на  Django REST Framework.
+
+### Как запустить проект:
+
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone git@github.com:AntonSpark/api_yatube.git
+```
+Нужно создать и активировать виртуальное окружение:
+```
+python3 -m venv venv
+```
+```
+source venv/Scripts/activate
+```
+
+Установить зависимости из файла requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+
+Выполнить миграции:
+```
+python manage.py migrate
+```
+
+Создать суперюзера
+```
+python manage.py createsuperuser
+```
+
+Запустить проект:
+```
+python manage.py runserver
+```
+
+Далее все запросы производятся в програме Postman
+Возможные Api запросы:
+```
+/api/v1/users/  (POST): передаём логин и пароль, /api/v1/create/получаем токен
+```
+```
+/posts/ (GET, POST): получаем список постов или создаём пост
+```
+```
+/posts/{post_id}/ (GET, PUT, PATCH, DELETE): получаем, редактируем или удаляем пост по id
+```
+```
+/groups/ (GET): получаем список групп
+```
+```
+/groups/{group_id}/ (GET): получаем информацию о группе по id
+```
+```
+/posts/{post_id}/comments/ (GET, POST): получаем список комментариев поста с id=post_id или создаём новый, указав id поста, который хотим прокомментировать
+```
+```
+/posts/{post_id}/comments/{comment_id}/ (GET, PUT, PATCH, DELETE): получаем, редактируем или удаляем комментарий по id
+```
+
+## Автор
+Антон Искров
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,68 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('__all__')
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        read_only=True
     )
+    post = serializers.ReadOnlyField(source='post.id')
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        read_only=False,
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate_following(self, following):
+        if self.context.get('request').user == following:
+            raise serializers.ValidationError(
+                'Подписка на себя невозможна'
+            )
+        return following
